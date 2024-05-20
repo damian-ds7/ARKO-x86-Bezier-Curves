fb4b7b79b55cb7a77da05d34f26d5d6592d0b219
Damian D'Souza <damian.dsouza@pw.edu.pl>
2024-05-20 15:37:29 +0200

    Fixed multiplication for 4+ point curve

--- bezier.s
+++ bezier.s
@@ -19,7 +19,6 @@ bezier:
 
 	mov r9d, 0xFF000000
 	mov rbx, rdx
-
 	movsxd r8, r8d
 
 begin:
@@ -28,12 +27,6 @@ begin:
 
 	cmp rsi, 1
 	je draw_points
-
-	sub rsp, 32
-	mov [rbp - 8], rsi
-	mov [rbp - 16], r8
-	mov [rbp - 24], rbx ; save x array on stack
-	mov [rbp - 32], rcx ; save y array on stack
 
 	movss xmm0, dword [zero] ; t
 	movss xmm1, dword [step] ; step
@@ -163,17 +156,23 @@ four_points_x: ; B(t) = (1 - t)^3*P0 + 3*(1 - t)^2*t*P1 + 3*(1 - t)*t^2*P2 + t^3
 	mulss xmm4, xmm2 ; (1 - t)^2*X0
 	mulss xmm4, xmm2 ; (1 - t)^3*X0
 
+	xorps xmm8, xmm8
+
 	mulss xmm5, xmm0 ; t*X1
 	mulss xmm5, xmm2 ; (1 - t)*t*X1
 	mulss xmm5, xmm2 ; (1 - t)^2*t*X1
+	addss xmm8, xmm5
 	addss xmm5, xmm5 ; 2*(1 - t)*t*X1
-	addss xmm5, xmm5 ; 3*(1 - t)*t*X1
+	addss xmm5, xmm8 ; 3*(1 - t)*t*X1
+
+	xorps xmm8, xmm8
 
 	mulss xmm6, xmm0 ; t*X2
 	mulss xmm6, xmm0 ; t^2*X2
 	mulss xmm6, xmm2 ; (1 - t)*t^2*X2
+	addss xmm8, xmm6
 	addss xmm6, xmm6 ; 2*(1 - t)*t^2*X2
-	addss xmm6, xmm6 ; 3*(1 - t)*t^2*X2
+	addss xmm6, xmm8 ; 3*(1 - t)*t^2*X2
 
 	mulss xmm7, xmm0 ; t*X3
 	mulss xmm7, xmm0 ; t^2*X3
@@ -191,25 +190,31 @@ four_points_y:
 	cvtsi2ss xmm6, [rcx + 8]
 	cvtsi2ss xmm7, [rcx + 12]
 
-	mulss xmm4, xmm2 ; (1 - t)*X0
-	mulss xmm4, xmm2 ; (1 - t)^2*X0
-	mulss xmm4, xmm2 ; (1 - t)^3*X0
+	mulss xmm4, xmm2 ; (1 - t)*Y0
+	mulss xmm4, xmm2 ; (1 - t)^2*Y0
+	mulss xmm4, xmm2 ; (1 - t)^3*Y0
 
-	mulss xmm5, xmm0 ; t*X1
-	mulss xmm5, xmm2 ; (1 - t)*t*X1
-	mulss xmm5, xmm2 ; (1 - t)^2*t*X1
-	addss xmm5, xmm5 ; 2*(1 - t)*t*X1
-	addss xmm5, xmm5 ; 3*(1 - t)*t*X1
+	xorps xmm8, xmm8
 
-	mulss xmm6, xmm0 ; t*X2
-	mulss xmm6, xmm0 ; t^2*X2
-	mulss xmm6, xmm2 ; (1 - t)*t^2*X2
-	addss xmm6, xmm6 ; 2*(1 - t)*t^2*X2
-	addss xmm6, xmm6 ; 3*(1 - t)*t^2*X2
+	mulss xmm5, xmm0 ; t*Y1
+	mulss xmm5, xmm2 ; (1 - t)*t*Y1
+	mulss xmm5, xmm2 ; (1 - t)^2*t*Y1
+	addss xmm8, xmm5
+	addss xmm5, xmm5 ; 2*(1 - t)*t*Y1
+	addss xmm5, xmm8 ; 3*(1 - t)*t*Y1
 
-	mulss xmm7, xmm0 ; t*X3
-	mulss xmm7, xmm0 ; t^2*X3
-	mulss xmm7, xmm0 ; t^3*X3
+	xorps xmm8, xmm8
+
+	mulss xmm6, xmm0 ; t*Y2
+	mulss xmm6, xmm0 ; t^2*Y2
+	mulss xmm6, xmm2 ; (1 - t)*t^2*Y2
+	addss xmm8, xmm6
+	addss xmm6, xmm6 ; 2*(1 - t)*t^2*Y2
+	addss xmm6, xmm8 ; 3*(1 - t)*t^2*Y2
+
+	mulss xmm7, xmm0 ; t*Y3
+	mulss xmm7, xmm0 ; t^2*Y3
+	mulss xmm7, xmm0 ; t^3*Y3
 
 	addss xmm4, xmm5
 	addss xmm4, xmm6
@@ -242,23 +247,26 @@ five_points_x: ; B(t) = (1 - t)^4*P0 + 4*(1 - t)^3*t*P1 + 6*(1 - t)^2*t^2*P2 + 4
 	mulss xmm5, xmm2 ; (1 - t)*t*X1
 	mulss xmm5, xmm2 ; (1 - t)^2*t*X1
 	mulss xmm5, xmm2 ; (1 - t)^3*t*X1
-	addss xmm5, xmm5 ; 2*(1 - t)*t*X1
-	addss xmm5, xmm5 ; 3*(1 - t)*t*X1
-	addss xmm5, xmm5 ; 4*(1 - t)*t*X1
+	addss xmm5, xmm5 ; 2*(1 - t)^3*t*X1
+	addss xmm5, xmm5 ; 4*(1 - t)^3*t*X1
+
+	xorps xmm9, xmm9
 
 	mulss xmm6, xmm0 ; t*X2
 	mulss xmm6, xmm0 ; t^2*X2
 	mulss xmm6, xmm2 ; (1 - t)*t^2*X2
 	mulss xmm6, xmm2 ; (1 - t)^2*t^2*X2
-	addss xmm6, xmm6 ; 2*(1 - t)*t^2*X2
-	addss xmm6, xmm6 ; 3*(1 - t)*t^2*X2
-	addss xmm6, xmm6 ; 4*(1 - t)*t^2*X2
+	addss xmm6, xmm6 ; 2*(1 - t)^2*t^2*X2
+	addss xmm9, xmm6
+	addss xmm6, xmm6 ; 4*(1 - t)^2*t^2*X2
+	addss xmm6, xmm9 ; 6*(1 - t)^2*t^2*X2
 
 	mulss xmm7, xmm0 ; t*X3
 	mulss xmm7, xmm0 ; t^2*X3
 	mulss xmm7, xmm0 ; t^3*X3
 	mulss xmm7, xmm2 ; (1 - t)*t^3*X3
-	addss xmm7, xmm7 ; 2*(1 - t
+	addss xmm7, xmm7 ; 2*(1 - t)*t^3*X3
+	addss xmm7, xmm7 ; 4*(1 - t)*t^3*X3
 
 	mulss xmm8, xmm0 ; t*X4
 	mulss xmm8, xmm0 ; t^2*X4
@@ -279,37 +287,40 @@ five_points_y:
 	cvtsi2ss xmm7, [rcx + 12]
 	cvtsi2ss xmm8, [rcx + 16]
 
-	mulss xmm4, xmm2 ; (1 - t)*X0
-	mulss xmm4, xmm2 ; (1 - t)^2*X0
-	mulss xmm4, xmm2 ; (1 - t)^3*X0
-	mulss xmm4, xmm2 ; (1 - t)^4*X0
+	mulss xmm4, xmm2 ; (1 - t)*Y0
+	mulss xmm4, xmm2 ; (1 - t)^2*Y0
+	mulss xmm4, xmm2 ; (1 - t)^3*Y0
+	mulss xmm4, xmm2 ; (1 - t)^4*Y0
 
-	mulss xmm5, xmm0 ; t*X1
-	mulss xmm5, xmm2 ; (1 - t)*t*X1
-	mulss xmm5, xmm2 ; (1 - t)^2*t*X1
-	mulss xmm5, xmm2 ; (1 - t)^3*t*X1
-	addss xmm5, xmm5 ; 2*(1 - t)*t*X1
-	addss xmm5, xmm5 ; 3*(1 - t)*t*X1
-	addss xmm5, xmm5 ; 4*(1 - t)*t*X1
+	mulss xmm5, xmm0 ; t*Y1
+	mulss xmm5, xmm2 ; (1 - t)*t*Y1
+	mulss xmm5, xmm2 ; (1 - t)^2*t*Y1
+	mulss xmm5, xmm2 ; (1 - t)^3*t*Y1
+	addss xmm5, xmm5 ; 2*(1 - t)^3*t*Y1
+	addss xmm5, xmm5 ; 4*(1 - t)^3*t*Y1
 
-	mulss xmm6, xmm0 ; t*X2
-	mulss xmm6, xmm0 ; t^2*X2
-	mulss xmm6, xmm2 ; (1 - t)*t^2*X2
-	mulss xmm6, xmm2 ; (1 - t)^2*t^2*X2
-	addss xmm6, xmm6 ; 2*(1 - t)*t^2*X2
-	addss xmm6, xmm6 ; 3*(1 - t)*t^2*X2
-	addss xmm6, xmm6 ; 4*(1 - t)*t^2*X2
+	xorps xmm9, xmm9
 
-	mulss xmm7, xmm0 ; t*X3
-	mulss xmm7, xmm0 ; t^2*X3
-	mulss xmm7, xmm0 ; t^3*X3
-	mulss xmm7, xmm2 ; (1 - t)*t^3*X3
-	addss xmm7, xmm7 ; 2*(1 - t
+	mulss xmm6, xmm0 ; t*XY2
+	mulss xmm6, xmm0 ; t^2*Y2
+	mulss xmm6, xmm2 ; (1 - t)*t^2*Y2
+	mulss xmm6, xmm2 ; (1 - t)^2*t^2*Y2
+	addss xmm6, xmm6 ; 2*(1 - t)^2*t^2*Y2
+	addss xmm9, xmm6
+	addss xmm6, xmm6 ; 4*(1 - t)^2*t^2*Y2
+	addss xmm6, xmm9 ; 6*(1 - t)^2*t^2*Y2
 
-	mulss xmm8, xmm0 ; t*X4
-	mulss xmm8, xmm0 ; t^2*X4
-	mulss xmm8, xmm0 ; t^3*X4
-	mulss xmm8, xmm0 ; t^4*X4
+	mulss xmm7, xmm0 ; t*Y3
+	mulss xmm7, xmm0 ; t^2*Y3
+	mulss xmm7, xmm0 ; t^3*Y3
+	mulss xmm7, xmm2 ; (1 - t)*t^3*Y3
+	addss xmm7, xmm7 ; 2*(1 - t)*t^3*Y3
+	addss xmm7, xmm7 ; 4*(1 - t)*t^3*Y3
+
+	mulss xmm8, xmm0 ; t*Y4
+	mulss xmm8, xmm0 ; t^2*Y4
+	mulss xmm8, xmm0 ; t^3*Y4
+	mulss xmm8, xmm0 ; t^4*Y4
 
 	addss xmm4, xmm5
 	addss xmm4, xmm6
@@ -323,12 +334,6 @@ five_points_draw:
 
 five_points_next:
 	inc_t five_points_x
-
-load_points:
-	mov rsi, [rbp - 8]
-	mov r8, [rbp - 16]
-	mov rbx, [rbp - 24]
-	mov rcx, [rbp - 32]
 
 draw_points:
 	xor r10, r10

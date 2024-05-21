9f4fe2bc5c77667a1e30bf896ef52bdbbb1b90e7
Damian D'Souza <damian.dsouza@pw.edu.pl>
2024-05-21 20:23:26 +0200

    Changed x and y from pointers to arrays

--- bezier.h
+++ bezier.h
@@ -1,6 +1,6 @@
 #ifndef __BEZIER_H_
 #define __BEZIER_H_
 
-void bezier(unsigned char *img, int counter, int *x, int *y, int pitch, int height);
+void bezier(unsigned char *img, int counter, int x[], int y[], int pitch, int height);
 
 #endif // __BEZIER_H_
